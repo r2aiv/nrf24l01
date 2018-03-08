0528a81b80c4a59e0d253411817898274672d9e6
Peter Majchrak <pm@lix.com>
2018-03-08 10:32:57 +0100

    Do something useful in the example

--- example/general/example.c
+++ example/general/example.c
@@ -1,6 +1,14 @@
+#include "main.h"
 #include "nrf24l01.h"
 
 extern SPI_HandleTypeDef hspi1;
+
+static void error() {
+    while (true) {
+        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        HAL_Delay(100);
+    }
+}
 
 void example() {
     nrf24l01 nrf;
@@ -11,51 +19,30 @@ void example() {
         config.tx_power         = NRF_TX_PWR_0dBm;
         config.crc_width        = NRF_CRC_WIDTH_1B;
         config.addr_width       = NRF_ADDR_WIDTH_5;
-        config.payload_length   = 32;   // maximum is 32 bytes
-        config.retransmit_count = 10;   // maximum is 15 times
+        config.payload_length   = 4;    // maximum is 32 bytes
+        config.retransmit_count = 15;   // maximum is 15 times
         config.retransmit_delay = 0x0F; // 4000us, LSB:250us
 
-        config.spi      = &hspi1;
-        config.ce_port  = GPIOA;
-        config.ce_pin   = GPIO_PIN_1;
-        config.irq_port = GPIOA;
-        config.irq_pin  = GPIO_PIN_2;
+        config.spi         = &hspi1;
+        config.spi_timeout = 10; // milliseconds
+        config.ce_port     = NRF_CE_GPIO_Port;
+        config.ce_pin      = NRF_CE_Pin;
+        config.irq_port    = NRF_IRQ_GPIO_Port;
+        config.irq_pin     = NRF_IRQ_Pin;
 
         nrf_init(&nrf, &config);
     }
 
-    bool send_packet = true;
+    uint32_t tx_data = 0xDEADBEEF;
+    uint32_t rx_data = 0;
+
+    nrf_send_packet(&nrf, (uint8_t*)&tx_data);
 
     while (true) {
-        if (send_packet) {
-            send_packet = false;
-        } else {
-        }
+        nrf_receive_packet(&nrf, (uint8_t*)&rx_data);
+        if (tx_data != rx_data) error();
+        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+        HAL_Delay(500);
+        HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
     }
-
-    // // block rx/tr
-    // if (true) {
-    //     static uint8_t    data[32] = "";
-    //     static NRF_RESULT res;
-    //     res = NRF_SendPacket(&nrf, data);
-    //
-    //     if (res == false) return;
-    //
-    //     res = NRF_ReceivePacket(&nrf, data);
-    //     if (res == false) return;
-    //
-    //     while (1) {
-    //         // There is means Tx/Rx is Okay.
-    //     }
-    // }
-    // // unblock Tx/Rx
-    // else {
-    //     static uint8_t data[32] = "";
-    //     NRF_PullPacket(&nrf, data);
-    //     // data will come later
-    //     while (1) {
-    //         // wait for data
-    //         HAL_Delay(1);
-    //     }
-    // }
 }
